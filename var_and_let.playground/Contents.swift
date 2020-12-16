aff9fb1b8f86d59136d75060dc1a39f017507b6c
dannylee93 <easternstar1005@gmail.com>
2020-12-16 23:14:28 +0900

    Upload files

---
+++ var_and_let.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+var year = 2018
+var valid  = false
+var x = 0.0, y = 1.0, z = 2.0
+
+str = "change data"
+year = year + 1
+print(str, year)
+
+let newName = "Lena"
+// newName = "change"  에러 발생
+print(newName)
+
+
