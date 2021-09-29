7e9923536e843673217fa3264e79f1e842288660
dongklee42 <easternstar1005@gmail.com>
2021-09-30 01:36:29 +0900

    Update files

--- learn-iOS/Book/TableViewSample/TableViewSample/ListViewController.swift
+++ learn-iOS/Book/TableViewSample/TableViewSample/ListViewController.swift
@@ -9,15 +9,16 @@ import UIKit
 
 class ListViewController: UITableViewController {
     // 데이터 세트화
-    var dataset = [
-        ("다크 나이트", "철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
-        ("호우시절", "때를 알고 내리는 비", "2009-10-08", 7.31, "rain.jpg"),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg"),
-    ]
+//    var dataset = [
+//        ("다크 나이트", "철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
+//        ("호우시절", "때를 알고 내리는 비", "2009-10-08", 7.31, "rain.jpg"),
+//        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg"),
+//    ]
     
     // 테이블 뷰를 구성할 리스트 데이터. 클로저를 이용해서 생략과 함께 데이터를 추가할 수 있다.
     lazy var list: [MovieVO] = {
         var dataList = [MovieVO]()
+        /*
         for (title, description,opendate, rating, thumbnail) in self.dataset {
             // 행 데이터 바인딩
             let mvo = MovieVO()
@@ -28,42 +29,44 @@ class ListViewController: UITableViewController {
             mvo.thumbnail = thumbnail
             dataList.append(mvo)
         }
+        */
         return dataList
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*  전부 삭제처리
-
-        // 첫 번째 행
-        var mvo = MovieVO()
-        mvo.title = "다크나이트"
-        mvo.description = "철학에 음악까지 더해져 예술이 되다."
-        mvo.opendate = "2008-09-04"
-        mvo.rating = 8.95
-        // 배열에 추가
-        self.list.append(mvo)
-        
-        // 두 번째 행
-        mvo = MovieVO()
-        mvo.title = "호우시절"
-        mvo.description = "때를 알고 내리는 비."
-        mvo.opendate = "2009-10-08"
-        mvo.rating = 7.31
-        // 배열에 추가
-        self.list.append(mvo)
-        
-        // 세 번째 행
-        mvo = MovieVO()
-        mvo.title = "말할 수 없는 비밀"
-        mvo.description = "여기서 너 까지 다섯 걸음."
-        mvo.opendate = "2015-05-07"
-        mvo.rating = 9.19
-        // 배열에 추가
-        self.list.append(mvo)
-        */
+        // 1. uri 생성
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=1&count=10&genreId=&order=releasedateasc"
+        let apiURI: URL! = URL(string: url)
+        // 2. REST API 호출. Data 객체활용
+        let apiData = try! Data(contentsOf: apiURI)
+        // 3. 데이터 전송결과 로그 출력
+        let log = NSString(data: apiData, encoding: String.Encoding.utf8.rawValue) ?? ""
+        NSLog("API Result == \(log)")
+        // 4. JSON 객체 파싱gotj NSDictionary 에 담기
+        do {
+            let apiDict = try JSONSerialization.jsonObject(with: apiData, options: []) as! NSDictionary
+            // 5. 데이터 구조에 맞춰서 차례대로 진입하면서 분리
+            let hoppin = apiDict["hoppin"] as! NSDictionary
+            let movies = hoppin["movies"] as! NSDictionary
+            let movie = movies["movie"] as! NSArray
+            // 6. 반복문 처리로 파싱한 데이터를 VO 객체에 넣는다.
+            for row in movie {
+                let r = row as! NSDictionary
+                let mvo = MovieVO()
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = ((r["ratingAverage"] as! NSString).doubleValue)
+                
+                // 프로퍼티에 추가
+                self.list.append(mvo)
+            }
+        } catch { }
     }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.list.count
     }
@@ -74,7 +77,7 @@ class ListViewController: UITableViewController {
         // 프로토타입셀에 붙인 이름을 활용해서 셀 객체를 큐로부터 가져옴
 //        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")! as! MovieCell
-        
+    
 //        // ---> 1. 커스텀셀로 만들면서 추가되는 부분
 ////        cell.textLabel?.text = row.title
 //        let title = cell.viewWithTag(101) as? UILabel
@@ -93,7 +96,13 @@ class ListViewController: UITableViewController {
         cell.desc?.text = row.description
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating)"
-        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        
+        // 수정
+//        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        let url: URL! = URL(string: row.thumbnail!)
+        let imageData = try! Data(contentsOf: url)
+        cell.thumbnail.image = UIImage(data: imageData)
+        
         // ---> 추가 끝
         
         return cell
@@ -101,6 +110,11 @@ class ListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은 \(indexPath.row) 번째 행입니다.")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.estimatedRowHeight = 80
+        self.tableView.rowHeight = UITableView.automaticDimension   // 사진 사이즈에 맞춰서 변경해야함
     }
     
     
